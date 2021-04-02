e8eae4499fcb1a80b95821140326e86b68335481
ananya-glitch <ananyapulastya123@gmail.com>
2021-04-01 23:38:16 -0400

    Adding project24

---
+++ PRO-c24-template2-main/sketch.js
@@ -0,0 +1,31 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var hammer;
+
+
+function setup(){
+    var canvas = createCanvas(1200,600);
+    engine = Engine.create();
+    world = engine.world;
+
+    plane = new Plane(600,height,1200,20)
+    hammer = new Hammer(10,100);
+    rubber = new Rubber(200, 290, 34);
+    iron = new Iron(400, 100, 300, 34);
+    stone = new Stone(500, 100, 300, 34);
+}
+
+function draw(){
+    background("lightBlue");
+    Engine.update(engine);
+
+
+    plane.display();
+    hammer.display();
+    rubber.display();
+    iron.display();
+    stone.display();
+}
